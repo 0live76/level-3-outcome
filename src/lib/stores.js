05d2318307116a8e72afb38e67241f0f2e62df9f
0live76 <roxburol76@craighead.school.nz>
2024-06-19 18:33:37 +1200

    Began work on importing arrays

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -5,20 +5,10 @@ export const user = writable("signedOut")
 export const isSignedIn = writable(false)
 export const orderStatus = writable("Ordering")
 
-
-
-
-export const orderStatus = writable([
-    { name: 'thing1', img: '/flour.jpg'}
-])
-
-export const orderStatus = writable([
+export const list = writable([
     { name: 'thing1'}
 ])
 
-export const orderStatus = writable([
-    'thing1',
-    'thing2'
-])
+
 
 
